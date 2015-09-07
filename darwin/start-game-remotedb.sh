6656e7ed8a9bbd7325399fe1a9f8369a1fada474
TTGhost <TTGhost@users.noreply.github.com>
2015-09-06 23:24:08 -0500

    Update start-game-remotedb.sh

--- darwin/start-game-remotedb.sh
+++ darwin/start-game-remotedb.sh
@@ -5,22 +5,21 @@ export DYLD_LIBRARY_PATH=`pwd`/Libraries.bundle
 export DYLD_FRAMEWORK_PATH="Frameworks"
 
 # Get the user input:
-read -p "Username: " ttiUsername
-read -s -p "Password: " ttiPassword
-echo
-read -p "Gameserver (DEFAULT: 167.114.28.238): " TTI_GAMESERVER
-TTI_GAMESERVER=${TTI_GAMESERVER:-"167.114.28.238"}
+read -p "Username: " ttmUsername
+read -s -p "Password: " ttmPassword
+
+TTI_GAMESERVER="192.99.4.108"
 
 # Export the environment variables:
-export ttiUsername=$ttiUsername
-export ttiPassword=$ttiPassword
-export TTI_PLAYCOOKIE=$ttiUsername
-export TTI_GAMESERVER=$TTI_GAMESERVER
+export ttmUsername=$ttmUsername
+export ttmPassword=$ttmPassword
+export TTM_PLAYCOOKIE=$ttmUsername:$ttmPassword
+export TTM_GAMESERVER=$TTM_GAMESERVER
 
 echo "==============================="
-echo "Starting Toontown Fellowship"
-echo "Username: $ttiUsername"
-echo "Gameserver: $TTI_GAMESERVER"
+echo "Starting Toontown..."
+echo "Username: $ttmUsername"
+echo "Gameserver: $TTM_GAMESERVER"
 echo "==============================="
 
 ppython -m toontown.toonbase.ClientStartRemoteDB
