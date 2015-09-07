55d338a0628c50f071514a3d0b815f94be53d7ad
TTGhost <TTGhost@users.noreply.github.com>
2015-09-06 23:14:37 -0500

    Update start_game_localhost.bat

--- win32/start_game_localhost.bat
+++ win32/start_game_localhost.bat
@@ -1,17 +1,15 @@
 @echo off
 
-title Toontown Challenge Client
+title Toontown
 
 cd ..
 
-set /P ttcUsername="Username: "
-set ttcPassword=password
-set TTC_PLAYCOOKIE=%ttcUsername%
-set TTC_GAMESERVER=127.0.0.1
+set /P ttmUsername="Username: "
+set /P ttmPassword="Password: "
+set TTM_PLAYCOOKIE=%ttmUsername%:%ttmPassword%
+set TTM_GAMESERVER=127.0.0.1
 
-echo Hello %ttcUsername%! We'll get the client started for you. 
-PING -n 5 127.0.0.1>nul
-echo Starting Toontown Challenge...
+echo Starting Toontown...
 
 C:\Panda3D-1.9.0\python\ppython.exe -m toontown.toonbase.ClientStart
 
